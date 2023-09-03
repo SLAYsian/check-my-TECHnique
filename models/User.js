a1c29a905d7dec0dd9fcb2fcc58c9118253ff5c2
slayonce <katharinemg@gmail.com>
2023-09-03 00:16:09 -0400

    fixed typo in user model id

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ class User extends Model {
 User.init(
   {
     id: {
-      DataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
